b0b6fe202a25ca8d5d4e671b09457d4522241b13
Sergey Menshikov <43649713+sergejoz@users.noreply.github.com>
2020-11-01 17:27:30 +0500

    Added simple work with s3 bucket (delete, add, view)

---
+++ AmazonS3/s3bucketapp/Models/ServiceConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace s3bucketapp.Models
+{
+    public class ServiceConfiguration
+    {
+        public AWSS3Configuration AWSS3 { get; set; }
+    }
+    public class AWSS3Configuration
+    {
+        public string BucketName { get; set; }
+    }
+}
